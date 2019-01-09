1b521f096ec1e368e4c87e87a2726951de993c4d
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2019-01-08 20:02:11 -0500

    New support for text input

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 
 class Test(object):
@@ -9,11 +10,17 @@ class Test(object):
         self._driver = webdriver.Chrome(driver_path)
         self._testfile = testfile
 
-    def _find_elemet(self,name,ref):
+    def _find_elemet(self,attr,attr_value):
         # getattr can return find_element_by_id or find_element_by_name function.
-        lookup = getattr(self._driver,'find_element_by_' + ref)
-        element = lookup(name)
+        lookup = getattr(self._driver,'find_element_by_' + attr)
+        element = lookup(attr_value)
         return element
+
+    def _action_type(self,action):
+        data, element_id = action.split(':') if ':' in action else None, action
+        field, value = element_id.split('=')
+        field_type, field_attr  = field.split('_')
+        return field_type
 
     def apply_test_cases(self):
         cases = self._testfile.cases
@@ -39,12 +46,26 @@ class Test(object):
         
         fields = action.split(':')
 
-        if len(fields) == 3:
-            text, name, ref = fields
-            box = self._find_elemet(name,ref)
-            box.send_keys(text)
+        if ':' in action:
+            # "my_user:text_name=userName"
+            data, element_id = action.split(':')
+            field, value = element_id.split('=')
+            field_type, field_attr  = field.split('_')
 
-        if len(fields) == 2:
-            name, ref = fields
-            button = self._find_elemet(name,ref)
+            set_function = getattr(self,'set_' + field_type)
+            set_function(data,field_attr,value)
+
+        if ':' not in action:
+            element, element_value = action.split('=')
+            element_type, element_attr  = element.split('_')
+            button = self._find_elemet(element_attr,element_value)
             button.click()
+
+    def set_text(self,data,attr_name,attr_value):
+        box = self._find_elemet(attr_name,attr_value)
+        box.send_keys(data)
+
+    def set_select(self,data,attr_name,attr_value):
+        select = self._find_elemet(attr_name,attr_value)
+        select = Select( select )
+        
