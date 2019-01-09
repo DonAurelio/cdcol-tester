c225875f4b64e7b73d80ea536f7dfaac668fe8fc
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2019-01-09 09:02:31 -0500

    Implemeenting apply run

--- test.py
+++ test.py
@@ -10,18 +10,6 @@ class Test(object):
         self._driver = webdriver.Chrome(driver_path)
         self._testfile = testfile
 
-    def _find_elemet(self,attr,attr_value):
-        # getattr can return find_element_by_id or find_element_by_name function.
-        lookup = getattr(self._driver,'find_element_by_' + attr)
-        element = lookup(attr_value)
-        return element
-
-    def _action_type(self,action):
-        data, element_id = action.split(':') if ':' in action else None, action
-        field, value = element_id.split('=')
-        field_type, field_attr  = field.split('_')
-        return field_type
-
     def apply_test_cases(self):
         cases = self._testfile.cases
         for case_name, case_data in cases.items():
@@ -29,6 +17,7 @@ class Test(object):
 
     def apply_test_case(self,case):
         self.apply_login(case)
+        self.apply_run(case)
 
     def apply_login(self,case):
         login = case.get('login',{})
@@ -37,6 +26,17 @@ class Test(object):
 
         self._driver.get(url)
         self.apply_actions(actions)
+
+    def apply_run(self,case):
+        run = case.get('run',{})
+        url = run.get('url','')
+        actions = run.get('actions',[])
+        finished = run.get('finished','')
+        collect = run.get('collect',[])
+
+        self._driver.get(url)
+        self.apply_actions(actions)
+        
 
     def apply_actions(self,actions):
         for action in actions:
@@ -49,23 +49,31 @@ class Test(object):
         if ':' in action:
             # "my_user:text_name=userName"
             data, element_id = action.split(':')
-            field, value = element_id.split('=')
-            field_type, field_attr  = field.split('_')
+            element, element_value = element_id.split('=')
+            element_type, element_attr  = element.split('_')
 
-            set_function = getattr(self,'set_' + field_type)
-            set_function(data,field_attr,value)
+            set_function = getattr(self,'set_' + element_type)
+            set_function(data,element_attr,element_value)
 
         if ':' not in action:
             element, element_value = action.split('=')
             element_type, element_attr  = element.split('_')
-            button = self._find_elemet(element_attr,element_value)
+            button = self._driver.find_element(element_attr,element_value)
             button.click()
 
     def set_text(self,data,attr_name,attr_value):
-        box = self._find_elemet(attr_name,attr_value)
+        box = self._driver.find_element(attr_name,attr_value)
         box.send_keys(data)
 
     def set_select(self,data,attr_name,attr_value):
-        select = self._find_elemet(attr_name,attr_value)
+        select = self._driver.find_element(attr_name,attr_value)
         select = Select( select )
+
+        if not data:
+            select.deselect_all()
+        else:
+            data = data.split(',')
+            for datum in data:
+                select.select_by_value(datum)
+
         
